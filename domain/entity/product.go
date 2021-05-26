957c285a72a2e4bca856314a8c56519ef470e0ba
Imtiaz Ahmed Riton <pro.imtiazahmedriton@gmail.com>
2021-05-26 22:55:26 +0600

    Update product.go

--- domain/entity/product.go
+++ domain/entity/product.go
@@ -1,7 +1,7 @@
 package entity
 
 type Product struct {
-	ID 		string  `json:"id"`
+	ID 	string  `json:"id"`
 	Title 	string  `json:"title"`
 	Amount 	int64   `json:"amount"`
 	Price 	float64 `json:"price"`
